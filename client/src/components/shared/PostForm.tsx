6d67f71cd05f683c29adbe446ec8358228e4f8f4
Lucifer13Freeman <Dmitri98LD@gmail.com>
2021-02-27 06:42:46 +0300

    commit-0.3

--- client/src/components/shared/PostForm.tsx
+++ client/src/components/shared/PostForm.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Quill from 'react-quill';
+import DOMPurify from 'dompurify';
 
 import { create } from '../../actions/post';
 
-class PostForm extends React.Component<any, any>
+
+const PostForm = ({ create }:any) =>
+{
+    const [body, set_body] = useState('');
+
+    const on_submit = (e:any) =>
+    {
+        e.preventDefault();
+        create({ body });
+        set_body('');
+    }
+
+    return(
+
+        <div className="card mb-4">
+            <div className="card-body">
+                <form onSubmit={on_submit}>
+                    <div className="form-group">
+                        <Quill
+                            placeholder="What's up?"
+                            theme="snow"
+                            modules={{
+                                toolbar: [
+                                    ['bold', 'italic', 'underline', 'strike'],
+                                    ['link', 'image', 'video'],
+                                    ['clean']
+                                ]
+                            }}
+                            value={body}
+                            onChange={value => set_body(DOMPurify.sanitize(value))}
+                        />
+                    </div>
+                    <div className="btn-group float-right">
+                        <button type="submit" className="btn btn-dark">Share</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+/*class PostForm extends React.Component<any, any>
 {
     constructor(props: any)
     {
@@ -56,7 +98,7 @@ class PostForm extends React.Component<any, any>
         auth: PropTypes.Validator<object>; 
     };
     
-}
+}*/
 
 PostForm.propTypes = {
     create: PropTypes.func.isRequired,
--- client/src/components/single-post/CommentForm.tsx
+++ client/src/components/single-post/CommentForm.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Quill from 'react-quill';
+import DOMPurify from 'dompurify';
 
 import { create_comment } from '../../actions/post';
 
-class CommentForm extends React.Component<any, any>
+
+const CommentForm = ({ post_id, create_comment }:any) =>
+{
+    const [body, set_body] = useState('');
+
+    const on_submit = (e:any) =>
+    {
+        e.preventDefault();
+        create_comment(post_id, { body });
+        set_body('');
+    }
+
+    return(
+        <div className="card mb-4">
+            <div className="card-body">
+                <form onSubmit={on_submit}>
+                    <div className="form-group">
+                        <Quill
+                            placeholder="What do you think?"
+                            theme="snow"
+                            modules={{ toolbar: false }}
+                            value={body}
+                            onChange={value => set_body(DOMPurify.sanitize(value))}
+                        />
+                    </div>
+                    <div className="btn-group">
+                        <button type="submit" className="btn btn-dark">Comment</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+/*class CommentForm extends React.Component<any, any>
 {
     constructor(props: any)
     {
@@ -52,7 +87,7 @@ class CommentForm extends React.Component<any, any>
         auth: PropTypes.Validator<object>; 
         post_id: PropTypes.Validator<string>; 
     };
-}
+}*/
 
 CommentForm.propTypes = {
     create_comment: PropTypes.func.isRequired,
